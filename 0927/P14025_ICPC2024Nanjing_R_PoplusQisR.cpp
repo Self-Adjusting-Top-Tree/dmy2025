a3735e14017a17e4eb3514f3db6ca6e7b774777f
gaaggaa <Email202401061258@163.com>
2025-10-01 18:14:43 +0800

    test

---
+++ 0927/P14025_ICPC2024Nanjing_R_PoplusQisR.cpp
@@ -0,0 +1,20 @@
+// #pragma GCC optimize(3, "Ofast", "inline", "unroll-loops")
+#include <bits/stdc++.h>
+#define int long long
+    #define dasongsong cin
+    #define ruahao cout
+    #define song18 cerr
+using namespace std;
+const int N = 1000010;
+char s[N];
+int pre[N];
+signed main()
+{
+    cin.tie(0)->sync_with_stdio(false);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        
+    }
+}
